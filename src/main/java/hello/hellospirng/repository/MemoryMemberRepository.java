96557960efa393a0542344258911162faa02f981
bruce <gotoarmy@naver.com>
2022-05-14 17:04:02 +0900

    서비스 테스트 케이스 만들기

--- src/main/java/hello/hellospirng/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospirng/repository/MemoryMemberRepository.java
@@ -36,4 +36,5 @@ public class MemoryMemberRepository implements MemberRepository{
         store.clear();
         sequence=0L;
     }
+
 }
--- src/test/java/hello/hellospirng/service/MemberserviceTest.java
+++ src/test/java/hello/hellospirng/service/MemberserviceTest.java
@@ -1,13 +1,15 @@
 package hello.hellospirng.service;
 
 import hello.hellospirng.domain.Member;
-import hello.hellospirng.repository.MemberRepository;
 import hello.hellospirng.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MemberserviceTest {
     MemoryMemberRepository REPO;
@@ -27,9 +29,13 @@ class MemberserviceTest {
     public void join() {
         //given
         Member member1 = new Member();
-        member1.setName("Hello");
+        member1.setName("Drake");
+        Member member2 = new Member();
+        member2.setName("Drk");
+
         //when
         Long ID = memberservice.join(member1);
+        memberservice.join(member2);
         //then
         Member findMem =REPO.findById(ID).get();
         assertEquals(member1.getName(),findMem.getName());
@@ -37,9 +43,37 @@ class MemberserviceTest {
 
     @Test
     public void findMembers() {
+        //given
+        Member member1 = new Member();
+        member1.setName("강동훈");
+        Member member2 = new Member();
+        member2.setName("강명철");
+        Member member3 = new Member();
+        member3.setName("장진남");
+        //when
+        memberservice.join(member1);
+        memberservice.join(member2);
+        memberservice.join(member3);
+        //then
+        List<Member> listmember = memberservice.findMembers();
+        assertEquals(listmember.size(),3);
+
     }
 
     @Test
     public void findOne() {
+        //given
+        Member member3 = new Member();
+        member3.setName("장진남");
+        Member member2 = new Member();
+        member2.setName("장첸");
+
+        //when
+        memberservice.join(member3);
+        memberservice.join(member2);
+        Member result = memberservice.findOne(member3.getId()).get();
+        //then
+        Assertions.assertThat(result).isEqualTo(member3);
+
     }
 }
